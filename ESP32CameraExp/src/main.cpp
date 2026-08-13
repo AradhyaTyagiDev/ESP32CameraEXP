b25cd8413c85c907fbba7ad3e6bc31d851811f8f
kuldeep <kuldeep.tyagi.vestel@gmail.com>
2026-08-14 02:29:38 +0530

    Fix the bug in the code

--- ESP32CameraExp/src/main.cpp
+++ ESP32CameraExp/src/main.cpp
@@ -6,13 +6,17 @@
 // Camera pin configuration
 // ============================================================
 
-#define PWDN_GPIO_NUM -1
-#define RESET_GPIO_NUM -1
-
+/// Sync
 #define XCLK_GPIO_NUM 15
+#define PCLK_GPIO_NUM 13
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM 7
+
+/// Camera Controls I2C
 #define SIOD_GPIO_NUM 4
 #define SIOC_GPIO_NUM 5
 
+/// Parallel Data Lines
 #define Y2_GPIO_NUM 11
 #define Y3_GPIO_NUM 9
 #define Y4_GPIO_NUM 8
@@ -22,9 +26,9 @@
 #define Y8_GPIO_NUM 17
 #define Y9_GPIO_NUM 16
 
-#define VSYNC_GPIO_NUM 6
-#define HREF_GPIO_NUM 7
-#define PCLK_GPIO_NUM 13
+/// GND, RESET
+#define PWDN_GPIO_NUM -1
+#define RESET_GPIO_NUM -1
 
 // ============================================================
 // Print camera status
@@ -268,7 +272,6 @@ bool initCamera()
     // --------------------------------------------------------
 
     config.pixel_format = PIXFORMAT_JPEG;
-
     // Start conservatively.
     // We will change this later.
     config.frame_size = FRAMESIZE_VGA;
